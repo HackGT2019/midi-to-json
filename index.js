aeefe2d7d1fe9ed59078abdce750c7b7982a375a
Bryce Plunkett <plunkett.bryce.m@gmail.com>
2019-10-26 10:59:46 -0400

    added an option to convert to wav

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const { Midi } = require('@tonejs/midi');
+const synth = require('synth-js');
+
 const argsv = require('yargs')
     .alias('p', 'path')
     .require('p', 'path required')
@@ -35,20 +37,30 @@ function loadMidi(pathToMidi, outputName, vocalTrack) {
         }
         return keep;
     });
-    writeOutputMidi(OUTPUT_PATH, outputName + '_NO_VOCALS', midi);
+    writeOutputMidiAndConvert(OUTPUT_PATH, outputName + '_NO_VOCALS', midi);
     processVocals(vocalTracks[0], midi, outputName); // process the vocals
-    console.log(`Completed`);
+    console.log('Completed');
 }
 
-function writeOutputMidi(filePath, name, midi) {
-    const path = filePath + name + '.mid';
-    fs.writeFileSync(path, new Buffer.from(midi.toArray()));
-    console.log(`Outputting midi to ${path}`)
+function writeOutputMidiAndConvert(filePath, name, midi) {
+    const fullPath = filePath + name + '.mid';
+    // eslint-disable-next-line no-undef
+    fs.writeFileSync(fullPath, new Buffer.from(midi.toArray()));
+    console.log(`Outputting midi to ${fullPath}`);
+    midiToWav(fullPath, filePath, name);
+
+}
+function midiToWav(pathToMidi, filePath, name) {
+    // const buffer = fs.readFileSync(pathToMidi);
+    // const wavBuffer = synth.midiToWav(buffer).toBuffer();
+    // const fullPath = filePath + name + '.wav';
+    // fs.writeFileSync(fullPath, wavBuffer, {encoding: 'binary'});
+    // console.log(`Outputting wav to ${fullPath}`);
 }
 
 function processVocals(vocalTrack, originalMidi, outputName) {
     originalMidi.tracks = [vocalTrack]; // use original midi because it has correct timing
-    writeOutputMidi(OUTPUT_PATH, outputName + '_VOCALS', originalMidi);
+    writeOutputMidiAndConvert(OUTPUT_PATH, outputName + '_VOCALS', originalMidi);
     const vocalTrackPath = OUTPUT_PATH + outputName + '.json';
     fs.writeFileSync(vocalTrackPath, JSON.stringify(originalMidi));
     console.log(`Writing vocal track to ${vocalTrackPath}`);
